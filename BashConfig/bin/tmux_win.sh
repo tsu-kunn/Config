b05554661078835a79b021e87c1575dbf9877d02
Tsuyoshi.A <perilla.k@gmail.com>
2022-12-04 14:14:44 +0900

    Update settings window in tmux

--- BashConfig/bin/tmux_win.sh
+++ BashConfig/bin/tmux_win.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
+if [ "$#" -eq 0 ]; then
+    # 新規Window
+    tmux new-window -n "work"
 
-# 新規Window
-tmux new-window -n "work"
+    # 画面分割
+    tmux split-window -v -p 75
+    tmux split-window -h -p 50
 
-# 画面分割
-tmux split-window -v -p 75
-tmux split-window -h -p 50
+    # 上部はtop、下部はgoto_projects実行
+    tmux send-keys -t 0 "top" C-m
+    tmux send-keys -t 1 "goto_projects Config" C-m
 
-# 上部はtop、下部はgoto_projects実行
-tmux send-keys -t 0 "top" C-m
-tmux send-keys -t 1 "goto_projects Config" C-m
+    tmux select-pane -t 2
 
-tmux select-pane -t 2
-
-# Windowを0に戻す
-tmux select-window -t 0
+    # Windowを0に戻す
+    tmux select-window -t 0
+else
+    case $1 in
+        t)
+            tmux split-window -v -p 75
+            tmux send-keys -t 0 "top" C-m
+            ;;
+        *)
+            echo [ERROR] "$1" は設定されていません。
+            ;;
+    esac
+fi
 
