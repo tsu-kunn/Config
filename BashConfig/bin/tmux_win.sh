d10ca5d27ccc4f853691b4549785118f11480bc9
Tsuyoshi.A <perilla.k@gmail.com>
2023-01-27 17:47:12 +0900

    Update tmux_win.sh

--- BashConfig/bin/tmux_win.sh
+++ BashConfig/bin/tmux_win.sh
@@ -21,6 +21,17 @@ else
             tmux split-window -v -p 75
             tmux send-keys -t 0 "top" C-m
             ;;
+        3)
+            # 画面分割
+            tmux split-window -v -p 75
+            tmux split-window -h -p 50
+
+            # 上部はtop、下部はgoto_projects実行
+            tmux send-keys -t 0 "top" C-m
+            tmux send-keys -t 1 "goto_projects IPMonitor_Tegra" C-m
+
+            tmux select-pane -t 2
+            ;;
         *)
             echo [ERROR] "$1" は設定されていません。
             ;;
