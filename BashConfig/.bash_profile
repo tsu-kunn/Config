3d977fb3f677e2eb3f80684090f4a964ef8ad2ab
Tsuyoshi.A <perilla.k@gmail.com>
2023-07-27 14:29:38 +0900

    Auto execute for tmux

--- BashConfig/.bash_profile
+++ BashConfig/.bash_profile
@@ -4,13 +4,16 @@ if [ -f ~/.bashrc ]; then
   . ~/.bashrc
 fi
 
-# 初回シェル時のみ tmux実行
-if [ $SHLVL = 1 ]; then
-  tmux
-fi
+# 初回シェル時のみtmux実行
+#if [ $SHLVL = 1 ]; then
+#  tmux
+#fi
 
-# 初回シェル時のみ Docker を実行
-if test $(service docker status | awk '{print $4}') = 'not'; then #停止状態
-  sudo service docker start
+# 1つだけtmux実行
+count=`ps aux | grep tmux | grep -v grep | wc -l`
+if test $count -eq 0; then
+    tmux
+elif test $count -eq 1; then
+    tmux a
 fi
 
--- BashConfig/bin/tmux_win.sh
+++ BashConfig/bin/tmux_win.sh
@@ -1,40 +1,18 @@
 #!/bin/bash
-if [ "$#" -eq 0 ]; then
-    # 新規Window
-    tmux new-window -n "work"
 
-    # 画面分割
-    tmux split-window -v -p 75
-    tmux split-window -h -p 50
+# 新規Window
+tmux new-window -n "work"
 
-    # 上部はtop、下部はgoto_projects実行
-    tmux send-keys -t 0 "top" C-m
-    tmux send-keys -t 1 "goto_projects Config" C-m
+# 画面分割
+tmux split-window -v -p 75
+tmux split-window -h -p 50
 
-    tmux select-pane -t 2
+# 上部はtop、下部はgoto_projects実行
+tmux send-keys -t 0 "top" C-m
+tmux send-keys -t 1 "goto_projects IPMonitor_Tegra" C-m
 
-    # Windowを0に戻す
-    tmux select-window -t 0
-else
-    case $1 in
-        t)
-            tmux split-window -v -p 75
-            tmux send-keys -t 0 "top" C-m
-            ;;
-        3)
-            # 画面分割
-            tmux split-window -v -p 75
-            tmux split-window -h -p 50
+tmux select-pane -t 2
 
-            # 上部はtop、下部はgoto_projects実行
-            tmux send-keys -t 0 "top" C-m
-            tmux send-keys -t 1 "goto_projects IPMonitor_Tegra" C-m
-
-            tmux select-pane -t 2
-            ;;
-        *)
-            echo [ERROR] "$1" は設定されていません。
-            ;;
-    esac
-fi
+# Windowを0に戻す
+# tmux select-window -t 0
 
